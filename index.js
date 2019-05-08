3de45afb9392fed31bcc74c58fa0d7bf80fb3782
Steven G. Harms <steven.harms@flatironschool.com>
2019-05-08 11:33:30 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i])
+  }
+  return r
+}
+
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i])
+  }
+  return r
+}
+
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i])
+  }
+  return r
+}
+
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i])
+  }
+  return r
+}
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
